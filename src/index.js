e8c2578fc0f504a8bbf7cf740a4b67b4344ae987
Jake Kim <jake@npgains.com>
2018-04-07 04:21:24 -0700

    Configure index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+import './css/normalize.css';
+import './css/app.scss';
+
+const app = <App/>;
+
+ReactDOM.render(app, document.getElementById('app-container'));
